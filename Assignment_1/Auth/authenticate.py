1490d9f2ca09115bd567db61ca0670d6a8b3999f
Pruthviraj Patil <pruthvir.patil98@gmail.com>
2022-11-04 14:42:39 -0400

    Assignment 1

---
+++ Assignment_1/Auth/authenticate.py
@@ -0,0 +1,10 @@
+class Authenticate:
+    def __init__(self):
+        self.key = "beBybSi8daPgsTp5yx5cHtHpYcrjp5Jq"
+        #self.key = "tO8IJApyCHaEYvTEEEidlb_hYfIQ1JUl"
+        
+
+    def getKey(self):
+        # The api key given by the professor
+        key = self.key
+        return key
